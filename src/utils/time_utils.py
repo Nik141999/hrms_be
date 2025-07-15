e6eb57366d8b37fc9cd124101228a5877d68da3a
Nikhil <saipatil141999@gmail.com>
2025-07-15 13:53:29 +0530

    fixing time tracker api

---
+++ src/utils/time_utils.py
@@ -0,0 +1,8 @@
+from datetime import timedelta
+
+def format_duration(duration: timedelta) -> str:
+    total_seconds = int(duration.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    seconds = total_seconds % 60
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
