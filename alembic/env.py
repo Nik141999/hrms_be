a1b2c5e79cef30f79869cea8c35ba1bf986d8f0c
Nikhil <saipatil141999@gmail.com>
2025-07-18 14:31:12 +0530

    migrations

--- alembic/env.py
+++ alembic/env.py
@@ -12,6 +12,8 @@ from src.models.organization import Organization
 from src.models.department import Department
 from src.models.time_tracker import TimeTracker
 from src.models.orgatization_type import OrganizationType
+from src.models.breakLog import BreakLog
+from src.models.workLog import WorkLog
 
 from src.database import Base
 
@@ -28,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata,
+target_metadata = Base.metadata
     
 
 # other values from the config, defined by the needs of env.py,
